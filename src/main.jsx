8b72ad9af0e2728f363a57708a14fa2f18f7001b
Mark Korzin <mark.korzin@gmail.com>
2025-12-25 12:53:43 +0200

    Getting data from API, handling loading, errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/app/App'
-import MarvelService from './services/MarvelService'
 import './style/style.scss'
-
-const marvelService = new MarvelService()
-marvelService.getAllCharacters().then(res => console.log(res))
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
